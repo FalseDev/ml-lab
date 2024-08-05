ca1f05b007c53ee6310015ef6a9caa4588cfbb3f
FalseDev <59121948+FalseDev@users.noreply.github.com>
2024-08-05 15:00:40 +0530

    Initial: Experiment 2

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+  in {
+    packages."x86_64-linux".default = pkgs.mkShell {
+
+      packages = with pkgs; [
+        (python311.withPackages (ps: with ps; [
+          numpy
+          pandas
+          matplotlib
+          seaborn
+          scikit-learn
+          opencv4
+
+          pyright
+          notebook
+        ]))
+      ];
+    };
+  };
+}
